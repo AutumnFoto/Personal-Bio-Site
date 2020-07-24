2e522ba154ef42adadabe68252700f9996809f8d
Autumn Fotopoulos <acarte94@gmail.com>
2020-07-24 17:42:07 -0500

    edited files

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ console.log("main.js, hey!")
 {
     title: "Cool Project";
     screenshot:"http://gotoflashgames.com/files/file/033.jpg";
-    description: "This is the best project"; // A good project description includes 'the what', 'the why', and 'the how'.
+    description: "Personal bio project for the first 6 months of NSS, front end."; 
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github";
     available: true;
-    url: "https://github.com/nss-evening-cohort-8/js-part-deux"; // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    url: "https://github.com/AutumnFoto/Personal-Bio-Site.git"; // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux";
   }
