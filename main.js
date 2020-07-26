373c875ccced184beed98832e20103a6cd5c231c
Autumn Fotopoulos <acarte94@gmail.com>
2020-07-26 14:07:42 -0500

    styling

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ const projects = [
       "Personal bio project for the first 6 months of NSS, front end.",
     technologiesUsed:
       "HTML, CSS, Vanilla JavaScript, Version Control with Github",
-    available: true,
+    available: false,
     url: "https://github.com/AutumnFoto/Personal-Bio-Site.git", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux",
   },
@@ -54,25 +54,24 @@ const printToDom = (divid, textToPrint) => {
   selectedDiv.innerHTML += textToPrint;
 };
 const createProjectCards = () => {
-  
   for (let i = 0; i < projects.length; i++) {
-    let domString = "";
-    domString += `<div class="project">`;
-    domString += `<h3>${projects[i].title}</h3>`;
-    domString += `<img src='${projects[i].screenshot}' />`;
-    domString += `<div>${projects[i].description}</div>`;
-    domString += `<div>${projects[i].technologiesUsed}</div>`;
-    domString += `<div>${projects[i].url}</div>`;
-    domString += `<div>${projects[i].githubUrl}</div>`;
-    domString += `</div>`;
-    printToDom("projectsPage", domString);
+    if (projects[i].available === true) {
+      let domString = "";
+      domString += `<div class="project">`;
+      domString += `<h3>${projects[i].title}</h3>`;
+      domString += `<img src='${projects[i].screenshot}' />`;
+      domString += `<div>${projects[i].description}</div>`;
+      domString += `<div>${projects[i].technologiesUsed}</div>`;
+      domString += `<div>${projects[i].url}</div>`;
+      domString += `<div>${projects[i].githubUrl}</div>`;
+      domString += `</div>`;
+      printToDom("projectsPage", domString);
+    }
   }
-
 };
 
 const init = () => {
-  createProjectCards();
-  
+  createProjectCards(projects);
 };
 
 init();
